21bfe06d7f5e423276131136ec340b12d83d1743
HuangHaoce <huang_haoce@163.com>
2018-03-30 11:12:15 +0800

    update works

--- src/works/one/ToToday.java
+++ src/works/one/ToToday.java
@@ -5,38 +5,20 @@ import java.text.ParseException;
 import java.util.*;
 
 public class ToToday {
-    public static Date getInputDate() throws ParseException {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请输入你要计算的日期：");
-        String line = sc.nextLine();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        Date d = sdf.parse(line);
-        return d;
+
+    public static void main(String [] args)throws ParseException {
+        String line = "2018-04-01";
+        System.out.print(line + "距离今天有");
+        System.out.print(ToToday.nextFolloupTimeFromnow(line));
+        System.out.print("天");
     }
 
-    // 接受输入，转成Date形式
-
-
-    // 计算Date距今多少天
-
-    // 输出多少天
-    public static long date(Date d) throws ParseException{
+    public static int nextFolloupTimeFromnow (String day) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        long thistime = d.getTime();
+        Date inputDay = sdf.parse(day);
         Date nowDate = sdf.parse(sdf.format(new Date()));
-        long nowtime = nowDate.getTime();
-        long time = nowtime-thistime;
-        long day = time/1000/60/60/24;
-        return Math.abs(day);
+        long time = inputDay.getTime() - nowDate.getTime();
+        return (int)(time/1000/60/60/24);
     }
 
-    public static void output(long day) {
-        System.out.println("距离今天有" +day+ "天");
-    }
-
-    public static void main(String [] args)throws ParseException{
-        Date input = ToToday.getInputDate();
-        long day = ToToday.date(input);
-        ToToday.output(day);
-    }
 }
--- src/works/two/DateTest.java
+++ src/works/two/DateTest.java
@@ -6,6 +6,7 @@ import java.util.*;
 import java.text.ParseException;
 
 public class DateTest {
+
     public static Date getInputDate() throws Exception{
         Scanner scan = new Scanner(System.in);
         System.out.println("请输入日期：");
@@ -20,27 +21,27 @@ public class DateTest {
         System.out.println("输入一个数字：");
         int num = scan.nextInt();
         return num;
-
     }
 
-    public static String date(Date d,int num){
-        Calendar c = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        c.setTime(d);
-        c.add(Calendar.DATE,num);
-        d = c.getTime();
-        String output = sdf.format(d);
-        return output;
-
+    public static String getDateAfterNDays(String basedDay,int num) throws ParseException {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date based = simpleDateFormat.parse(basedDay);
+        calendar.setTime(based);
+        calendar.add(Calendar.DATE, num);
+        return simpleDateFormat.format(calendar.getTime());
     }
 
     public static void output(int num,String outdate) {
         System.out.println(num+ "天后是" +outdate);
     }
+
     public static void main(String [] args) throws Exception{
-       Date inputdate = DateTest.getInputDate();
-       int n = DateTest.getInputNum();
-       String out = DateTest.date(inputdate,n);
+//       Date inputdate = DateTest.getInputDate();
+//       int n = DateTest.getInputNum();
+        String inputdate = "2018-04-01";
+        int n = 2;
+       String out = DateTest.getDateAfterNDays(inputdate,n);
        DateTest.output(n,out);
     }
 }
