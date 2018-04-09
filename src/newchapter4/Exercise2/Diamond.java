aad1adf698ae66ef87293f217dcce7218cfe1baa
yeying0121 <yeying0121@gmail.com>
2018-04-09 15:34:38 +0800

    add exercise of chapter 4 and 5

--- src/newchapter4/Exercise2/Diamond.java
+++ src/newchapter4/Exercise2/Diamond.java
@@ -1,4 +1,25 @@
 package newchapter4.Exercise2;
 
 public class Diamond {
+    public static void main(String[] args) {
+        int h = 4;
+        for(int i=0; i<h; i++){
+            for(int j=0; j<h-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0; k<2*i+1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=0; i<h; i++) {
+            for(int j=0;j<=i+1; j++) {
+                System.out.print(" ");
+            }
+            for(int k=0; k<(h-1)*2-2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
